72f397a61de22085c2615e4357f95ff1cd77d3c6
Anna Makarova <foxygoldy84@gmail.com>
2026-09-01 15:39:48 +0300

    "fix: resolve bootstrap static connection and templates configuration"

--- core/settings.py
+++ core/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'catalog' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -115,6 +115,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# Указываем Django, где искать папку static
+STATICFILES_DIRS = [
+    BASE_DIR / 'catalog' / 'static',
+]
 
 
 # Email
